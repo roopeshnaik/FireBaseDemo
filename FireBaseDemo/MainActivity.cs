227cfaa6cf50e3ffb1315ee04b121adf021c65bc
Roopesh Naik <rnaik77@stanford.edu>
2017-08-29 14:07:03 +0530

    Add project files.

---
+++ FireBaseDemo/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Firebase.Analytics;
+
+namespace FireBaseDemo
+{
+    [Activity(Label = "FireBaseDemo", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private FirebaseAnalytics firebaseAnalytics;
+        private Button btnMystuff, btnDigest;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            firebaseAnalytics = FirebaseAnalytics.GetInstance(this);
+
+            //btnMystuff = FindViewById<Button>(Resource.)
+        }
+    }
+}
+
